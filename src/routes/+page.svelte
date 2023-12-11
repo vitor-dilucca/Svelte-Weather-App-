89a46f91ab76b149916aae20c0240cd86fa61da4
Vitor Dilucca <vitordilucca@hotmail.com>
2023-12-11 15:37:36 -0300

    modificando pagina principal

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Pagina teste Svelte</h1>
+
+<style>
+	h1 {
+		font-family:
+			system-ui,
+			-apple-system,
+			BlinkMacSystemFont,
+			'Segoe UI',
+			Roboto,
+			Oxygen,
+			Ubuntu,
+			Cantarell,
+			'Open Sans',
+			'Helvetica Neue',
+			sans-serif;
+	}
+</style>
