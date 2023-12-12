963ef9153f2ba08d9a869c88a1f4e1adb386b23d
Vitor Dilucca <vitordilucca@hotmail.com>
2023-12-12 14:00:44 -0300

    separando em outro componente com o JS, deestructurando os dados da API

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,36 +1,6 @@
 <script>
-	const options = {
-		method: 'GET',
-		headers: {
-			'X-RapidAPI-Key': 'ff2e7b6e27mshf571c7cc1c215c6p1d46dcjsn6656ca81a515',
-			'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
-		}
-	};
-	const weatherPromise = fetch(
-		'https://weatherapi-com.p.rapidapi.com/current.json?q=Goiânia',
-		options
-	)
-		.then((response) => response.json())
-		.then((response) => {
-			const { location, current } = response;
-			const { country, localtime, name } = location;
-			const { condition, humidity, feelslike_c, is_day, temp_c, wind_kph, wind_dir } = current;
-			const { code, text } = condition;
-
-			return {
-				conditionCode: code,
-				conditionText: text,
-				country,
-				localtime,
-				name,
-				humidity,
-				isDay: is_day,
-				feelslike: feelslike_c,
-				temperature: temp_c,
-				windSpeed: wind_kph,
-				windDir: wind_dir
-			};
-		});
+	import {getWeatherFrom} from '../services/weather.js'
+	const weatherPromise = getWeatherFrom()
 </script>
 
 {#await weatherPromise then weather}
@@ -38,8 +8,6 @@
 		{weather.conditionText}
 	</h1>
 {/await}
-
-<h1>Pagina teste Svelte</h1>
 
 <style>
 	h1 {
---
+++ src/services/weather.js
@@ -0,0 +1,32 @@
+	const FETCH_OPTIONS = {
+		method: 'GET',
+		headers: {
+			'X-RapidAPI-Key': 'ff2e7b6e27mshf571c7cc1c215c6p1d46dcjsn6656ca81a515',
+			'X-RapidAPI-Host': 'weatherapi-com.p.rapidapi.com'
+		}
+	};
+
+	export async function getWeatherFrom(query='Goiânia'){
+		const response = await fetch(
+			`https://weatherapi-com.p.rapidapi.com/current.json?q=${query}`,
+			FETCH_OPTIONS
+		)
+        const data = await response.json()
+		const { location, current } =data;
+		const { country, localtime, name } =location;
+		const { condition, humidity, feelslike_c, is_day, temp_c, wind_kph, wind_dir } = current;
+		const { code, text } = condition;
+		return {
+			conditionCode: code,
+			conditionText: text,
+			country,
+			localtime,
+			name,
+			humidity,
+			isDay: is_day,
+			feelslike: feelslike_c,
+			temperature: temp_c,
+			windSpeed: wind_kph,
+			windDir: wind_dir
+		};
+}
